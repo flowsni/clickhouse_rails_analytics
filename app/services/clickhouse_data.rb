d214d692a3623378f4f081c5d6339af0fc2299f4
Dima <naumkindp@mail.ru>
2019-06-12 19:55:38 +0300

    add navbar

--- app/services/clickhouse_data.rb
+++ app/services/clickhouse_data.rb
@@ -2,10 +2,10 @@ class ClickhouseData
   def self.q1
     #countries = `echo 'SELECT DayOfWeek, count(*) AS c FROM ontime WHERE Year >= 2000 AND Year <= 2008 GROUP BY DayOfWeek ORDER BY c DESC FORMAT JSONEachRow' | curl 'http://default:123@127.0.0.1/?output_format_json_quote_64bit_integers=1' -d @-`
     countries = `echo 'SELECT DayOfWeek, count(*) AS c FROM datasets.ontime WHERE Year >= 2006 AND
-     Year <= 2018 GROUP BY DayOfWeek ORDER BY c DESC FORMAT JSON' |
-     curl 'http://default:123@localhost:8123/' -d @-`
+      Year <= 2018 GROUP BY DayOfWeek ORDER BY c DESC FORMAT JSON' |
+      curl 'http://default:123@localhost:8123/' -d @-`
 
-    data = JSON.parse(countries)['data']
+      data = JSON.parse(countries)['data']
     sorted_data = data.sort_by { |k| k['DayOfWeek'] }
     days = []
     counts = []
